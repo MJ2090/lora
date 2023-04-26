cb1aeb57d47780599f4341d601022358b91a81f3
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-04-26 16:25:01 -0700

    s

--- my_finetune.py
+++ my_finetune.py
@@ -33,3 +33,40 @@ DEVICE
 df = pd.read_csv("bitcoin-sentiment-tweets.csv")
 print(df.head())
 print(df.sentiment.value_counts())
+
+def sentiment_score_to_name(score: float):
+    if score > 0:
+        return "Positive"
+    elif score < 0:
+        return "Negative"
+    return "Neutral"
+ 
+dataset_data = [
+    {
+        "instruction": "Detect the sentiment of the tweet.",
+        "input": row_dict["tweet"],
+        "output": sentiment_score_to_name(row_dict["sentiment"])
+    }
+    for row_dict in df.to_dict(orient="records")
+]
+ 
+print(dataset_data[0])
+import json
+with open("alpaca-bitcoin-sentiment-dataset.json", "w") as f:
+   json.dump(dataset_data, f)
+
+BASE_MODEL = "decapoda-research/llama-7b-hf"
+ 
+model = LlamaForCausalLM.from_pretrained(
+    BASE_MODEL,
+    load_in_8bit=True,
+    torch_dtype=torch.float16,
+    device_map="auto",
+)
+ 
+tokenizer = LlamaTokenizer.from_pretrained(BASE_MODEL)
+ 
+tokenizer.pad_token_id = (
+    0  # unk. we want this to be different from the eos token
+)
+tokenizer.padding_side = "left"
