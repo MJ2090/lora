8bfe65ab57ee68f2ee7410c00aa160b72080c17a
mj <125602296+MJ2090@users.noreply.github.com>
2023-05-02 09:49:53 -0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -23,7 +23,7 @@ echo "LORA ended ================================"
 
 echo "BASH started =============================="
 echo 'export LD_LIBRARY_PATH=:/usr/lib/x86_64-linux-gnu/' >> ~/.bashrc
-echo 'alias g3="python3"' >> ~/.bashrc
+echo 'alias p3="python3"' >> ~/.bashrc
 echo 'alias gl="git pull"' >> ~/.bashrc
 . ~/.bashrc
 echo "BASH ended ================================"
