51199002b4351dc4505898f03e6fd2bf6df7edbd
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-05-02 17:12:09 -0700

    s

--- generate_chat.py
+++ generate_chat.py
@@ -1,16 +1,11 @@
 import os
 import sys
-
 import fire
 import gradio as gr
 import torch
-import transformers
 from peft import PeftModel
 from transformers import GenerationConfig, LlamaForCausalLM, LlamaTokenizer
-
-from utils.callbacks import Iteratorize, Stream
 from utils.prompter import Prompter
-import random
 import time
 
 
