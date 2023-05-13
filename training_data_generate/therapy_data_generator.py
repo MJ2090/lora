528041839bb0b5d598b4fa4f9e485062e422a6c7
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-05-12 17:18:24 -0700

    s

--- training_data_generate/therapy_data_generator.py
+++ training_data_generate/therapy_data_generator.py
@@ -1,9 +1,12 @@
 import openai
+import json
+import re
 from multiprocessing import Pool, freeze_support
 
 
-GPT_4 = 'gpt-4'
-GPT_3_5 = 'gpt-3.5-turbo'
+MODEL_GPT_4 = 'gpt-4'
+MODEL_GPT_3_5 = 'gpt-3.5-turbo'
+
 
 class Dialogue():
 
@@ -12,56 +15,62 @@ class Dialogue():
         self.char_visitor = char_visitor
         self.history = []
 
-    def call_openai(self, messages, model=GPT_3_5):
+    def call_openai(self, messages, model=MODEL_GPT_4):
         response = openai.ChatCompletion.create(
             model=model,
             temperature=0.8,
-            max_tokens=3600,
+            max_tokens=200,
             messages=messages,
         )
         # print(f"message sent to openai ====== \n\n{messages}\n\n")
         return response
 
-
     def run_therapist(self):
-        char_therapist = "Assume you are a professional Therapist, the user is a Visitor to you. You guide the visitor by asking questions based on their backgroud and experiences, instead of pvoviding lots of suggestions. The Visitor recently lost their job."
-        messages = [{"role": "system", "content": char_therapist}]
+        messages = [{"role": "system", "content": self.char_therapist}]
         for item in self.history:
-            if item.get('role') == 'visitor':
-                messages.append({"role": "assistant", "content": item['content']})
+            if item['role'] == 'therapist':
+                messages.append(
+                    {"role": "assistant", "content": item['content']})
             else:
                 messages.append({"role": "user", "content": item['content']})
 
         openai_response = self.call_openai(messages)
         ai_message = openai_response["choices"][0]["message"]["content"]
-        print(f"\n{ai_message}\n")
+        print(f"Therapist: \n{ai_message}\n")
         self.history.append({'role': 'therapist', 'content': ai_message})
 
-
     def run_visitor(self):
-        char_visitor = "Assume you are a Visitor, and the user is a Therapist. You recently lost your job."
-        messages = [{"role": "system", "content": char_visitor}]
+        messages = [{"role": "system", "content": self.char_visitor}]
         for item in self.history:
-            if item.get('role') == 'therapist':
-                messages.append({"role": "assistant", "content": item['content']})
+            if item['role'] == 'visitor':
+                messages.append(
+                    {"role": "assistant", "content": item['content']})
             else:
                 messages.append({"role": "user", "content": item['content']})
 
         openai_response = self.call_openai(messages)
         ai_message = openai_response["choices"][0]["message"]["content"]
-        print(f"Visitorrr: \n{ai_message}\n")
+        print(f"Visitor: \n{ai_message}\n")
         self.history.append({'role': 'visitor', 'content': ai_message})
 
-
     def generate_dialogue(self):
-        self.history = []
-        for i in range(4):
+        self.history = [{'role': 'therapist',
+                         'content': "Hello, how can I help you today?"}]
+        for _ in range(10):
             self.run_visitor()
             self.run_therapist()
-    
 
-if __name__=="__main__":
-    t = "Assume you are a professional Therapist, the user is a Visitor to you. The Visitor recently lost their job."
-    v = "Assume you are a Visitor, and the user is a Therapist. You recently lost your job."
+
+if __name__ == "__main__":
+    t = """Assume you are a professional Therapist, the user is a Visitor to you. The Visitor recently lost their job.
+    You talk and guide the Visitors based on these rules:
+    1. Ask questions based on Visitor's expreiences and backbrounds, guding them to think and find solutions by themselves.
+    2. Do not provide lots of regular suggestions, that is usually helpless.
+    3. One question at a time, do not ask multiple questions together.
+    """
+    v = """Assume you are a Visitor, and the user is a Therapist. Your backgrounds are:
+    1. You are at your 20s.
+    2. You have lost your job recently.
+    3. You are a little nervous visiting the therapist, who you do not know much about."""
     d = Dialogue(char_therapist=t, char_visitor=v)
     d.generate_dialogue()
